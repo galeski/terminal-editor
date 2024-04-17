3e361bb313894e79cef4c5e22d2d45cd9cd2b115
Mateusz Galeski <mgaleski@gmail.com>
2024-04-17 16:57:27 +0200

    Add rudimentary key echoing, set up for further text editor development

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+debug = true;
 const fs = require('fs');
 const path = require('path');
+const keypress = require('keypress');
 
 // Specify the file path
-const filePath = '/home/mg/Documents/fizzbuzz.js'; // Replace with the actual path to your file
+const filePath = '/home/mg/Documents/testfile';
 
 // fs.readFile(filePath, 'utf8', (err, data) => {
 //   if (err) {
@@ -17,6 +19,7 @@ class FileBuffer {
   constructor(filePath) {
     this.filePath = String(filePath);
     this.fileContent;
+    this.dirty = false;
   }
   readFile = function(filePath, encoding) {
     try {
@@ -37,27 +40,120 @@ class CreateView {
     this.fileName = path.parse(FileBuffer.filePath).name;
     this.fileType = path.parse(FileBuffer.filePath).ext;
     this.fullName = path.parse(FileBuffer.filePath).base;
+    this.cursorPos = [1, 2];
   }
-  out = function(arg) {
-    process.stdout.write(arg);
-  }
-  clearScreen = function() {
-    this.out("\u001b[2J");
-  }
-  moveCursor = function(x, y) {
+  updateCursorPos = function(x, y) {
+    if (x < 1 || y < 1) return;
+    this.cursorPos = [x, y];
   }
 }
 
-let newFile = new FileBuffer(filePath);
-newFile.readFile(newFile.filePath);
-newFile.printFileContent();
-console.log(newFile.fileContent);
+const out = (arg) => {
+  process.stdout.write(arg);
+}
 
-let newView = new CreateView(newFile);
-console.log(newView.out("test"));
+const moveCursor = (x, y) => {
+  x = x || 0;
+  y = y || 0;
+  out('\u001b[' + y + ';' + x + 'H');
+}
 
-setTimeout(function () {
-  console.log('boo')
-}, 1000000);
+const updateFilename = (arg) => {
+  moveCursor(1, 1);
+  out("Current filename: " + String(arg) + "\n");
+}
 
-newView.clearScreen();
+const clearScreen = () => {
+  out("\u001b[2J");
+}
+
+const clearLine = () => {
+  out("\33[2K\r");
+}
+
+const updateCursor = (view, a = 0, b = 0) => {
+  const x = view.cursorPos[0];
+  const y = view.cursorPos[1];
+  moveCursor(x + a, y + b);
+  view.updateCursorPos(x + a, y + b);
+}
+
+clearScreen();
+updateFilename(filePath);
+
+let editedFile = new FileBuffer(filePath);
+let currentView = new CreateView(editedFile);
+editedFile.readFile(editedFile.filePath);
+editedFile.printFileContent();
+moveCursor(1, 2);
+
+// make `process.stdin` begin emitting "keypress" events
+keypress(process.stdin);
+
+// listen for the "keypress" event
+process.stdin.on('keypress', function (ch, key) {
+  if (key && debug) {
+    let prevCursorPosX = currentView.cursorPos[0];
+    let prevCursorPosY = currentView.cursorPos[1];
+    moveCursor(1, 1);
+    clearLine();
+    console.log('got keypress: ', key.name);
+    moveCursor(prevCursorPosX, prevCursorPosY);
+  }
+  if (key) editedFile.dirty = true;
+
+  if (key && key.ctrl && key.name == 'c') {
+    clearScreen();
+    moveCursor(1, 1);
+    if (editedFile.dirty) out("File was edited.");
+    process.stdin.pause();
+  } else if (key && key.name == 'up') {
+    updateCursor(currentView, 0, -1);
+  } else if (key && key.name == 'down') {
+    updateCursor(currentView, 0, +1);
+  } else if (key && key.name == 'left') {
+    updateCursor(currentView, -1, 0);
+  } else if (key && key.name == 'right') {
+    updateCursor(currentView, +1, 0);
+  } else {
+    if (key.name != "backspace") {
+      out(key.name);
+      // move cursor one to the right, fixes debug logging
+      updateCursor(currentView, +1, 0);
+    }
+    else updateCursor(currentView, -1, 0);
+    // console.log(key);
+  }
+
+});
+
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
+
+// mainloop = function() {
+//   while(true) {
+//     let newFile = new FileBuffer(filePath);
+//     newFile.readFile(newFile.filePath);
+//     // console.log(newFile.fileContent);
+//     let newView = new CreateView(newFile);
+//     process.stdin.setRawMode(true)
+//     newView.clearScreen();
+//     newView.moveCursor(0, 0)
+//     newFile.printFileContent();
+//     // process.stdin.on('data', (data) => {
+//     //   let input = data.toString().trim();
+//     //   if (input === 'x') {
+//     //     process.exit()
+//     //   }
+//     // });
+//   }
+// }
+// mainloop()
+
+// console.log(newView.out("test"));
+
+// setTimeout(function () {
+//   console.log('boo')
+// }, 1000000);
+
